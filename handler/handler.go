d4db5ebca062b4eae0bf13ae1fdc829bf592a274
eaglesunshine <18900927316@163.com>
2020-09-30 23:22:29 +0800

    update code

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,17 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
+
+	"filestore-server/meta"
+	"filestore-server/util"
 )
 
 /*
@@ -33,19 +39,29 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+
 		//创建一个本地的文件来接收文件流
-		newFile, err := os.Create("/tmp/" + head.Filename) //创建文件
+		newFile, err := os.Create(fileMeta.Location) //创建文件
 		if err != nil {
 			fmt.Printf("Failed to create file, err:%s", err.Error())
 			return
 		}
 		defer newFile.Close()
 
-		_, err = io.Copy(newFile, file) //文件复制，返回复制的字节长度、错误信息
+		fileMeta.FileSize, err = io.Copy(newFile, file) //文件复制，返回复制的字节长度、错误信息
 		if err != nil {
 			fmt.Printf("Failed to save data into file, err:%s", err.Error())
 			return
 		}
+
+		newFile.Seek(0, 0) //Seek设置下一次读/写的位置。offset为相对偏移量，而whence决定相对位置：0为相对文件开头，1为相对当前位置，2为相对文件结尾。
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
 
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound) //上传完成，重定向到上传成功的网页url
 	}
@@ -54,4 +70,104 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 //UploadSuchHandler: 上传已完成
 func UploadSuchHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Upload finished")
+}
+
+//GetFileMetaHandler: 获取文件元信息
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //解析
+
+	filehash := r.Form["filehash"][0]
+	fMeta := meta.GetFileMeta(filehash)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+//FileQueryHandler: 查询批量的文件元信息
+func FileQueryHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	limitCnt, _ := strconv.Atoi(r.Form.Get("limit"))
+	fileMetas := meta.GetLastFileMetas(limitCnt)
+	data, err := json.Marshal(fileMetas)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+//DownloadHandler: 根据输入的filehash下载对应文件
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	//解析请求参数
+	r.ParseForm()
+	fsha1 := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(fsha1)
+
+	//读取文件
+	f, err := os.Open(fm.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octect-stream")
+	//MediaType，即是Internet Media Type，互联网媒体类型；也叫做MIME类型，在Http协议消息头中，使用Content-Type来表示具体请求中的媒体类型信息。
+	w.Header().Set("Content-Disposition", "attachment; filename=\""+fm.FileName+"\"")
+	//Content-disposition其实可以控制用户请求所得的内容存为一个文件的时候提供一个默认的文件名，文件直接在浏览器上显示或者在访问时弹出文件下载对话框。
+	w.Write(data)
+}
+
+//FileMetaUpdateHandler: 更新文件元信息(重命名)
+func FileMetaUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	opType := r.Form.Get("op")
+	fileSha1 := r.Form.Get("filehash")
+	newFileName := r.Form.Get("filename")
+
+	if opType != "0" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	curFileMeta := meta.GetFileMeta(fileSha1)
+	curFileMeta.FileName = newFileName
+	meta.UpdateFileMeta(curFileMeta)
+
+	data, err := json.Marshal(curFileMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+//FileDeleteHandler: 删除文件以及元信息
+func FileDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fileSha1 := r.Form.Get("filehash")
+
+	fMeta := meta.GetFileMeta(fileSha1)
+	os.Remove(fMeta.Location)
+
+	meta.RemoveFileMeta(fileSha1)
+
+	w.WriteHeader(http.StatusOK)
 }
--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func main() {
 	//网页js、css等静态资源服务
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	http.HandleFunc("/file/upload", handler.UploadHandler) //设定路由规则
+	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc", handler.UploadSuchHandler)
+	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
+	http.HandleFunc("/file/query", handler.FileQueryHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
+	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
+	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+
 	err := http.ListenAndServe("127.0.0.1:8888", nil) //设定监听端口，这里设置为监听所有网卡的8080端口
 	if err != nil {
 		fmt.Printf("Failed to start server, err:%s", err.Error())
